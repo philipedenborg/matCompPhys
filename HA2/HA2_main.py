0f9ea66648f6de4f1a72a87c886f5c2045b8a0d2
philipedenborg <philip.edenborg@gmail.com>
2018-02-01 10:55:27 +0100

    Added parallel argument to calc

--- HA2/HA2_main.py
+++ HA2/HA2_main.py
@@ -11,8 +11,8 @@ p = 0.10
 
 for k in range(4,30,2):
 	# create calculator			
-	calc = GPAW(mode = fd,	# Basis with Cut-off			
-			h = 0.18,	
+	calc = GPAW(mode = PW(500),	# Basis with Cut-off			
+			parallel={'band':1},	
 			kpts = (k,k,k),		# Number of k-points
 			xc = 'PBE',		# xc functional
 			txt = 'Al-%02d.txt' % k,	# Name of output file
