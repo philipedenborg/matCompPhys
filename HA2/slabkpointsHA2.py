552a6dbe85d0797e34224a778b8034043170f1de
fantmagnus <fantm@ft4011-09.ft4011.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2018-02-02 16:41:07 +0100

    Routine to calculate relevant slab k-points for fcc111 and fcc100 surfaces

---
+++ HA2/slabkpointsHA2.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from ase import Atoms
+from gpaw import GPAW, PW
+from ase.build import fcc111
+from ase.build import fcc100
+from ase.parallel import rank
+import numpy as np
+
+a0 = 4.04
+slab1 = fcc111('Al',size = (20,20,3), a = a0)
+
+for k in range(4,30,2):
+	# create calculator			
+	calc = GPAW(mode = PW(500),	# Basis with Cut-off			
+			parallel={'band':1},	
+			kpts = (k,k,k),		# Number of k-points
+			xc = 'PBE',
+			basis = 'dzp',		# xc functional
+			txt = 'Al-slab1%02d.txt' % k,	# Name of output file
+			eigensolver = 'rmm-diis')
+	slab1.set_calculator(calc)
+	E = slab2.get_potential_energy()	
+	if rank == 0:	
+		print k,E	
+
+slab2 = fcc100('Al',size = (20,20,3), a = a0)
+
+for k in range(4,30,2):
+	# create calculator			
+	calc = GPAW(mode = PW(500),	# Basis with Cut-off			
+			parallel={'band':1},	
+			kpts = (k,k,k),		# Number of k-points
+			xc = 'PBE',
+			basis = 'dzp',		# xc functional
+			txt = 'Al-slab2%02d.txt' % k,	# Name of output file
+			eigensolver = 'rmm-diis')
+	slab2.set_calculator(calc)
+	E = slab2.get_potential_energy()	
+	if rank == 0:	
+		print k,E	
+
+
