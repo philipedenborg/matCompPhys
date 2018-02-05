e0d618ddf9a8b1868fe0f36a8cc11186ea8b1a7e
fantmagnus <fantm@f7203-12.f7203.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2018-02-05 20:49:39 +0100

    Forgot to commit this before

--- HA2/slabkpointsHA2.py
+++ HA2/slabkpointsHA2.py
@@ -11,20 +11,18 @@ N = 5
 fcc = fcc100('Al', (1, 1, N), a=a0, vacuum=7.5)
 fcc.center(axis=2)
 
-for k in range(5,50,5):
+for N in range(9,13,2):
 	# create calculator			
 	calc = GPAW(mode = PW(650),	# Basis with Cut-off			
 			parallel={'band':1},	
 			kpts = (k,k,1),		# Number of k-points
 			xc = 'PBE',
 			basis = 'dzp',		# xc functional
-			txt = 'Al-slab5.txt',	# Name of output file
+			txt = 'slab-%d.txt' %N,	# Name of output file
 			eigensolver = 'rmm-diis')
 	fcc.set_calculator(calc)
 	E = fcc.get_potential_energy()	
-	if rank == 0:	
-		print k,E
-
+	
 
 
 	
