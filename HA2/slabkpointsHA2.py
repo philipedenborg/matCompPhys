c5004a86e21246c65260e3d7b959c6ecf4265658
fantmagnus <fantm@ft4011-09.ft4011.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2018-02-06 13:11:03 +0100

    Proper calc. of surface

--- HA2/slabkpointsHA2.py
+++ HA2/slabkpointsHA2.py
@@ -7,11 +7,11 @@ from ase.parallel import rank
 import numpy as np
 
 a0 = 4.04
-N = 5
-fcc = fcc100('Al', (1, 1, N), a=a0, vacuum=7.5)
-fcc.center(axis=2)
+k = 16 
 
-for N in range(9,13,2):
+for N in range(1,23,2):
+        fcc = fcc100('Al', (1, 1, N), a=a0, vacuum=7.5)
+        fcc.center(axis=2)
 	# create calculator			
 	calc = GPAW(mode = PW(650),	# Basis with Cut-off			
 			parallel={'band':1},	
