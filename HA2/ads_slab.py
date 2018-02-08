7d324e5f3d5c56b424a040704320e043c3f9f3ae
philipedenborg <philip.edenborg@gmail.com>
2018-02-08 16:43:06 +0100

    Small changes to ads_slab

--- HA2/ads_slab.py
+++ HA2/ads_slab.py
@@ -6,17 +6,20 @@ from ase.optimize import QuasiNewton
 from ase.build import fcc111,fcc100, add_adsorbate
 from ase.parallel import rank
 
-h = 1.85
-d = 1.16
-k = 16
-N = 1
+h = 1.85	# Off-set of adsorbate
+d = 1.16	# Bond length of adsorbate
+k = 5		# Number of k-points
+N = 1		# Number of cell layers in slab
+vac = 10.0	# Vacuum 
 
-surface = 100
-site = "ontop"
+surface = 100	# 100 or 111
+site = "ontop"  # ontop, bridge or hollow for 100 and ontop or bridge for 111 (fcc or hcp)
 pos = [(0., 0., 0.), (0., 0., d)] # Use to set orientation
 
 if surface == 100:
-	slab = fcc111('Al', size=(1, 1, N), vacuum=10.0)
+	slab = fcc100('Al', size=(1, 1, N), vacuum=vac)
+if surface == 111:
+	slab = fcc111('Al', size=(1, 1, N), vacuum=vac)
 
 calc1 = GPAW(mode = PW(650),	# Basis with Cut-off			
 			parallel={'band':1},	
@@ -37,7 +40,7 @@ calc2 = GPAW(mode='lcao',        # use the LCAO basis mode
             txt='out.txt')      # name of GPAW output text file
 
 molecule = Atoms('CO', positions = pos )
-a = 8
+a = 8				# Size of molecule cell
 molecule.set_cell((a,a,a))
 molecule.center()
 
@@ -51,5 +54,5 @@ slab.set_constraint(constraint)
 dyn = QuasiNewton(slab, trajectory='AlCO.traj')
 dyn.run(fmax=0.05)
 
-if rank==0:
-    print('Adsorption energy:', e_slab + e_CO - slab.get_potential_energy())
+if rank==0:	
+    print(surface,site,N,'Adsorption energy:', e_slab + e_CO - slab.get_potential_energy())
