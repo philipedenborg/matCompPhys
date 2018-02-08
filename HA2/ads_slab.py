cc8c07c88a9dbccd18a270093eb15d256574c67b
philipedenborg <philip.edenborg@gmail.com>
2018-02-08 16:24:01 +0100

    Small changes to ads_slab.py

--- HA2/ads_slab.py
+++ HA2/ads_slab.py
@@ -9,43 +9,46 @@ from ase.parallel import rank
 h = 1.85
 d = 1.16
 k = 16
-N = 5
+N = 1
 
-slab = fcc111('Al', size=(1, 1, N), vacuum=10.0)
+surface = 100
+site = "ontop"
+pos = [(0., 0., 0.), (0., 0., d)] # Use to set orientation
 
-calc = GPAW(mode = PW(650),	# Basis with Cut-off			
+if surface == 100:
+	slab = fcc111('Al', size=(1, 1, N), vacuum=10.0)
+
+calc1 = GPAW(mode = PW(650),	# Basis with Cut-off			
 			parallel={'band':1},	
 			kpts = (k,k,1),		# Number of k-points
 			xc = 'PBE',
 			basis = 'dzp',		# xc functional
-			txt = 'slab-%d.txt' %N,	# Name of output file
+			txt = 'ads-%d-%s-%d.txt' %(surface,site,N),	# Name of output file
 			eigensolver = 'rmm-diis')
 
-slab.set_calculator(calc)
+slab.set_calculator(calc1)
+e_slab = slab.get_potential_energy()
 
-
-calc1 = GPAW(mode='lcao',        # use the LCAO basis mode
+calc2 = GPAW(mode='lcao',        # use the LCAO basis mode
             basis='dzp',        # use double zeta polarized basis set
             h=0.18,             # grid spacing
             xc='PBE',           # XC-functional
             #kpts=(16,16,16),    # k-point grid
             txt='out.txt')      # name of GPAW output text file
 
-
-e_slab = slab.get_potential_energy()
-
-molecule = Atoms('CO', positions=[(0., 0., 0.), (0., 0., d)])
+molecule = Atoms('CO', positions = pos )
 a = 8
 molecule.set_cell((a,a,a))
 molecule.center()
 
-molecule.set_calculator(calc1)
+molecule.set_calculator(calc2)
 e_CO = molecule.get_potential_energy()
 
-add_adsorbate(slab, molecule, h, 'ontop')
+
+add_adsorbate(slab, molecule, h, site)
 constraint = FixAtoms(mask=[a.symbol == 'Al' for a in slab])
 slab.set_constraint(constraint)
-dyn = QuasiNewton(slab, trajectory='N2Cu.traj')
+dyn = QuasiNewton(slab, trajectory='AlCO.traj')
 dyn.run(fmax=0.05)
 
 if rank==0:
