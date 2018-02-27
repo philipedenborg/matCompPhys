87880b1d2f4877d63485b45aabf7e5c26f8289db
philipedenborg <philip.edenborg@gmail.com>
2018-02-27 18:50:56 +0100

    analyse HA2

--- HA2/data/HA2analyse.py
+++ HA2/data/HA2analyse.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+from ase.io import read
 
 res = ''
 plt.rc('text',usetex=True)
 plt.rc('font', family='serif')
 
 while res != '0':
-    print "K-points = kp, Cut off  = cf, Equation of state = eos, Surface energies = surf. Write 0 to exit"
+    print "K-points = kp, Cut off  = cf, Equation of state = eos, Surface energies = surf, E_ads convergence = adsconv. Write 0 to exit"
     res = raw_input("What are you looking at today?\n")
 
     if res == 'cf':
@@ -19,21 +20,45 @@ while res != '0':
             cf.append(col)
         cf = np.array(cf)
         plt.figure()
-        plt.plot(cf[:,0],cf[:,1])
+        plt.plot(cf[:,0],cf[:,1],'-o')
         plt.rc('text',usetex=True)
         plt.rc('font', family='serif') 
-        plt.xlabel('Cut-off energy [eV]',fontsize=10)
-        plt.ylabel('Energy [eV]',fontsize=10) 
+        plt.xlabel('Cut-off energy [eV]',fontsize=16)
+        plt.ylabel('Energy [eV]',fontsize=16) 
+        ax = plt.gca()
+	ax.ticklabel_format(useOffset=False)
+        plt.grid(True)		
         plt.show()
+
     elif res == 'eos':
         V = 16.529
         a = (4*V)**(1/3.0)
         B = 77.708
-        print "Lattice constant: %.3f Å" % a
+        print "Lattice constant: %.5f Å" % a
         print "Bulkmodulus: B %.3f GPa" % B  
         img=mpimg.imread('Al-eos.png')
-        imgplot = plt.imshow(img)
-        plt.show()
+        #imgplot = plt.imshow(img)
+        #plt.show()
+        
+        configs = read('Al.traj@0:5')  # read 5 configurations
+	# Extract volumes and energies:
+	volumes = [al.get_volume() for al in configs]
+	energies = [al.get_potential_energy() for al in configs]
+        v = np.array(volumes)
+	E = np.array(energies)
+	degree = 4 # Degree of polynomial fit
+	p = np.polyfit(v,E,degree)
+	x = np.linspace(v[0],v[4],200)
+	y = np.polyval(p,x)
+
+	plt.figure()
+	plt.plot(x,y)
+	plt.hold(True)
+	plt.plot(v,E,'bo')
+	plt.xlabel('Volume [\AA]',fontsize=16)
+        plt.ylabel('Energy [eV]',fontsize=16)
+        plt.grid(True)
+	plt.show()
     elif res == 'kp':
         f = open('k_file','r')
         data = []
@@ -50,44 +75,30 @@ while res != '0':
         plt.rc('font', family='serif')
         plt.xlabel('Number of k-points',fontsize=16)
         plt.ylabel('Energy [eV]',fontsize=16)
+        plt.grid(True)
         plt.show()
-    elif res == 'surf':
-        
-        
-        # fcc(100)
-        A = 1
-        E_slab = 1
-        N_slab = 1
-        N_bulk = 1
-        E_bulk = 1
-
-        sigma100 = 1/(2*A)*(E_slab - N_slab/N_bulk * E_bulk)
-        # fcc(111)
-        A = 1
-        E_slab = 1
-        N_slab = 1
-        N_bulk = 1
-        E_bulk = 1
-         
-        sigma111 = 1/(2*A)*(E_slab - N_slab/N_bulk * E_bulk)
-
-        
-        f = open('k_file','r')
+    elif res == 'adsconv':    
+        f = open('conv100_OT_O_energy','r')
         data = []
         for line in f:
             string = line.split(' ')
             col = [float(s) for s in string]
             data.append(col)
         data = np.array(data)
-        k = data[:,0]
+        N = data[:,0]
         E = data[:,1]
         plt.figure(1)
-        plt.plot(k,E,'-o')
+        plt.plot(N,E,'-o')
         plt.rc('text',usetex=True)
         plt.rc('font', family='serif')
-        plt.xlabel('Number of k-points',fontsize=16)
-        plt.ylabel('Energy [eV]',fontsize=16)
+        plt.xlabel('Number of cell layers',fontsize=16)
+        plt.ylabel('Adsorption energy [eV]',fontsize=16)
+        ax = plt.gca()
+	ax.ticklabel_format(useOffset=False)
+	plt.grid(True)
         plt.show()
-        
+        print E[2]
+    elif res == 'surf':
+
 
 
